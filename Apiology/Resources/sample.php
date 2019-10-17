2d0c66d4399c11e62165901444b8b2b052b53968
Jorge <jorge@algorithmlab.co>
2019-10-17 18:51:27 -0300

    Sample class with methods
    Updated Sample Class with Main method as default, and 2 other methods. First one without arguments and second one with arguments

--- Apiology/Resources/sample.php
+++ Apiology/Resources/sample.php
@@ -10,14 +10,43 @@ use Apiology\Classes\{Headers};
 
  	public function main()
  	{
- 		echo parent::success_200();
-
+ 		if($_SERVER['REQUEST_METHOD'] == 'GET')
+ 		{
+ 			parent::get_header(200, false, null);
+ 			$this->http_response["status"] = "OK YES";
+ 			echo json_encode($this->http_response, JSON_PRETTY_PRINT);
+ 		}
  	}
 
- 	public function sample_method($_args)
+ 	public function sample_method_one()
  	{
 
- 		parent::success_200();
- 		echo $_POST['post_key'] . ' and ' . $_args[0];
+ 		parent::get_header(200, false, null);
+		$this->http_response["status_code"] = 200;
+		$this->http_response["message"] = "JSON Method Message";
+		echo json_encode($this->http_response, JSON_PRETTY_PRINT);
+ 	}
+
+ 	public function sample_method_two()
+ 	{
+
+ 		if(func_num_args() < 1)
+ 		{
+ 			parent::get_header(400, true, "I don't really know what you are asking me for");
+ 		}
+ 		else
+ 		{
+ 			parent::get_header(200, false, null);
+
+			$this->http_response["status_code"] = 200;
+			$this->http_response["message"] = "JSON Method Message";
+
+			foreach (func_get_args() as $value) {
+ 				$this->http_response["parameters"]= $value; 
+ 			}
+
+			echo json_encode($this->http_response, JSON_PRETTY_PRINT);
+ 		}
+
  	}
  }
