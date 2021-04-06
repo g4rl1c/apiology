df3939a0f20f8b531aa6e597543b69047c157fb1
gorka-lab <dev@bytelab.cl>
2021-04-06 14:10:38 -0400

    CORS to everyone and Added GET, PUT, POST and DELETE Methods

--- api/index.php
+++ api/index.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: https://actcontadores.cl");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Allow: GET, POST");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Allow: GET, POST, PUT, DELETE");
 
 header('Content-Type: application/json');
 
