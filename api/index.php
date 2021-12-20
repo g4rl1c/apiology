75ec8b32130ff02f97e86d652d1c2f9702acef68
Gorka-Lab <dev@algorithmlab.co>
2021-12-20 11:31:33 -0300

    v2.1

--- api/index.php
+++ api/index.php
@@ -1,15 +1,28 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Allow: GET, POST, PUT, DELETE");
-header('Content-Type: application/json');
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept, X-SC-Api-Key");
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
-include_once './App/vendor/autoload.php';
-require_once 'App/init.php';
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-// Namespace Constants
-define("APIOLOGY", "App/");
-define("RESOURCES", "App/Resources/");
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		// may also be using PUT, PATCH, HEAD etc
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-new Apiology\Init();
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept, X-Gtm-Api-Key");
+
+	exit(0);
+}
+
+require 'vendor/autoload.php';
+
+use Apiology\Apiology\init as Init;
+
+new Init();
